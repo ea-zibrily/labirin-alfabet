cde03f4115c1a3fa93c29a6573a8501421f02d5a
Zibril Yuan <zyuuannn@gmail.com>
2024-03-21 11:57:47 +0700

    refactor: frame rate manager

--- Assets/_Project/Script/Managers/FrameRateManager.cs
+++ Assets/_Project/Script/Managers/FrameRateManager.cs
@@ -5,7 +5,7 @@ using Alphabet.DesignPattern.Singleton;
 
 namespace Alphabet.Managers
 {
-    public class FrameRateManager : MonoDDOL<FrameRateManager>
+    public class FrameRateManager : MonoSingleton<FrameRateManager>
     {
         #region Fields & Properties
         
@@ -15,13 +15,14 @@ namespace Alphabet.Managers
         
         #endregion
 
-        #region Methods
+        #region MonoBehaviour Callbacks
 
-        // !-- Initialization
-        public override void InitComponent() 
+        protected override void Awake() 
         {
             if (!isFixedFrameRate) return;
+            
             Application.targetFrameRate = targetFrameRate;
+            Debug.Log(Application.targetFrameRate);
         }
 
         #endregion
